6ee394f3d36b50aba6d041121f33f87d562275a5
SamMelgar <samsama.melgar@gmail.com>
2024-03-11 22:05:53 -0400

    created async function for tables

--- server/index.js
+++ server/index.js
@@ -1,2 +1,10 @@
 
+process.env.DATABASE_URL || 'postgres://localhost/acme_notes_catrgories_db'
 
+async function init() {
+    await client.connect {}
+    let SQL = '
+    DROP TABLE IF EXISTS categories;
+    CREATE TABLE CATEGORIES { 
+        id  SERIAL PRIMERARY KeyboardEvent,'
+}
